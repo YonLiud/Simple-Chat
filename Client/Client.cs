b1df3ee34461ff5080c1e930727a93903ab3ab9e
Yon Liud <45634836+YonLiud@users.noreply.github.com>
2020-08-11 21:29:00 +0300

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace Client
+{
+    public class Client : BaseScript
+    {
+        public Client()
+        {
+            EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
+            EventHandlers["clientTweet"] += new Action<string, string>(clientTweet);
+            EventHandlers["clientAction"] += new Action<string, string>(clientAction);
+        }
+        private void OnClientResourceStart(string resourceName)
+        {
+            RegisterCommand("twt", new Action<int, List<object>, string>((source, args, raw) =>
+            {
+                if (args.Count < 1)
+                {
+                    TriggerEvent("chat:addMessage", new
+                    {
+                        color = new[] { 255, 0, 0 },
+                        multiline = true,
+                        args = new[] { "Oops", $"Did you mean to type your message too?" }
+                    });
+                    return;
+                }
+                string text = "";
+                text = string.Join(" ", args.ToArray());
+                string playername = Game.Player.Name;
+                TriggerServerEvent("serverTweet", playername, text);
+            }), false);
+            RegisterCommand("me", new Action<int, List<object>, string>((source, args, raw) =>
+            {
+                if (args.Count < 1)
+                {
+                    TriggerEvent("chat:addMessage", new
+                    {
+                        color = new[] { 255, 0, 0 },
+                        multiline = true,
+                        args = new[] { "Oops", $"Did you mean to type your message too?" }
+                    });
+                    return;
+                }
+                string text = "";
+                text = string.Join(" ", args.ToArray());
+                string playername = Game.Player.Name;
+                TriggerServerEvent("serverAction", playername, text);
+            }), false);
+
+        }
+        private void clientTweet(string name, string args)
+        {
+            TriggerEvent("chat:addMessage", new
+            {
+                color = new[] { 0, 204, 204 },
+                multiline = true,
+                args = new[] { "Tweeter🐦 "/*<< BIRB <<*/, $" {name} > {args}" }
+            });
+            return;
+        }
+        private void clientAction(string name, string args)
+        {
+            TriggerEvent("chat:addMessage", new
+            {
+                color = new[] { 255, 87, 87 },
+                multiline = true,
+                args = new[] { "Action", $" ^*{name}:  {args}" }
+            });
+            return;
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace Server
+{
+    public class Server : BaseScript
+    {
+        public Server()
+        {
+            EventHandlers["serverTweet"] += new Action<string, string>(serverTweet);
+            EventHandlers["serverAction"] += new Action<string, string>(serverAction);
+        }
+        private void serverTweet(string name, string args)
+        {
+            TriggerClientEvent("clientTweet", name, args);
+        }
+        private void serverAction(string name, string args)
+        {
+            TriggerClientEvent("clientAction", name, args);
+        }
+    }
+}
